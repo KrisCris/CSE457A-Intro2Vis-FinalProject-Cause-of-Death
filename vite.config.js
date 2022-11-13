006e16523144636d54002165f4eaf9e70dbe03a3
connlost <hi.imhpc@outlook.com>
2022-11-13 16:20:25 -0600

    add @rollup/plugin-dsv

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 import { resolve } from 'path';
+import dsv from '@rollup/plugin-dsv'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    dsv(),
     createSvgIconsPlugin({
       iconDirs: [resolve(process.cwd(), 'src/assets/icons')],
       symbolId: 'icon-[dir]-[name]',
