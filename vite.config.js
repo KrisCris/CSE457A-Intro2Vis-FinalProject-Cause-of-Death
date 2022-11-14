2fe07a3a1f6af4c1497d5f24fc2102099868a637
connlost <hi.imhpc@outlook.com>
2022-11-14 17:22:49 -0600

    try fix url overwritten issue

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,18 @@ import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 import { resolve } from 'path';
 import dsv from '@rollup/plugin-dsv';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: 'causeofdeath',
-  plugins: [
-    vue(),
-    dsv(),
-    createSvgIconsPlugin({
-      iconDirs: [resolve(process.cwd(), 'src/assets/icons')],
-      symbolId: 'icon-[dir]-[name]',
-    }),
-  ],
-});
+export default ({ mode }) => {
+  return defineConfig({
+    base: mode === 'production'
+      ? '/causeofdeath/'
+      : '/',
+    plugins: [
+      vue(),
+      dsv(),
+      createSvgIconsPlugin({
+        iconDirs: [resolve(process.cwd(), 'src/assets/icons')],
+        symbolId: 'icon-[dir]-[name]',
+      }),
+    ],
+  })
+}
