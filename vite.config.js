1ca07b5f8f7d10399071ba22aab2d9a1e4f0e4ea
connlost <hi.imhpc@outlook.com>
2022-11-14 19:10:31 -0600

    clean up

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,14 @@ import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 import { resolve } from 'path';
 import dsv from '@rollup/plugin-dsv';
 
-export default ({ ...mode }) => {
-  console.log(mode)
-  return defineConfig({
-    base: '',
-    plugins: [
-      vue(),
-      dsv(),
-      createSvgIconsPlugin({
-        iconDirs: [resolve(process.cwd(), 'src/assets/icons')],
-        symbolId: 'icon-[dir]-[name]',
-      }),
-    ],
-  })
-}
+export default defineConfig({
+  base: '',
+  plugins: [
+    vue(),
+    dsv(),
+    createSvgIconsPlugin({
+      iconDirs: [resolve(process.cwd(), 'src/assets/icons')],
+      symbolId: 'icon-[dir]-[name]',
+    }),
+  ],
+});
