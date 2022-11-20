f56fd5b35d47ea1a894247aaad1c98936372b6cb
connlost <hi.imhpc@outlook.com>
2022-11-20 02:34:59 -0600

    stacked bar

--- src/pages/StackedBar/StackedBar.vue
+++ src/pages/StackedBar/StackedBar.vue
@@ -1,25 +1,14 @@
 <script setup>
+import { max } from 'd3-array';
+import { scaleBand, scaleLinear, scaleSqrt, scaleOrdinal } from 'd3-scale';
 import { select } from 'd3-selection';
 import { stack } from 'd3-shape';
 import { ref, onMounted } from 'vue';
 import stackStore from '../../stores/stack'
+import worldStore from '../../stores/world'
 
 const stackState = stackStore();
-
-const wrapper = ref();
-const svg = ref();
-
-onMounted(() => {
-  const { width } = wrapper.value.getBoundingClientRect();
-  stackState.init(width);
-  draw();
-});
-
-const draw = () => {
-  const data = stack().keys([
-
-  ])
-}
+const worldState = worldStore();
 
 const years = [];
 
@@ -27,79 +16,210 @@ for (let i = 1990; i < 2020; i++) {
   years.push(i);
 }
 
+const hover = ref(null);
+
+const wrapper = ref();
+const svg = ref();
+const tooltip = ref();
+
+const reasons = stackState.getReasonNames();
+
+const colorSet = []
+for (let i = 0; i < reasons.length; ++i) {
+  colorSet.push(worldState.getRandomColor())
+}
+
+const colorScale = scaleOrdinal(colorSet).domain(reasons);
+let barGrp = undefined;
+
+onMounted(() => {
+  const { width } = wrapper.value.getBoundingClientRect();
+  stackState.init(width);
+  draw();
+});
+
+const onChangeYear = (year) => {
+  stackState.setYear(year);
+  draw()
+}
+
+const draw = async () => {
+  barGrp = barGrp || select(svg.value).append("g");
+
+  const rawData = Object.values(stackState.getYearlyData());
+  rawData.sort((a, b) => b.total_death - a.total_death)
+  const countryNames = rawData.map(d => d.country_name)
+
+  const y = scaleBand()
+      .domain(countryNames)
+      .range([0, stackState.width * 3])
+      .padding([0.2])
+
+  const x = scaleSqrt()
+    .domain([0, max(rawData, d => d.total_death)])
+    .range([0, stackState.width - 100]);
+
+  const data = stack().keys(stackState.getReasonNames())(rawData)
+  barGrp
+    .attr("transform", `translate(${50}, ${0})`)
+    .selectAll("g")
+    .data(data)
+    .join("g")
+    .attr("fill", d => colorScale(d.key))
+    .selectAll("rect")
+    .data(d => d)
+    .join(
+      enter => enter
+        .append("rect")
+        .attr("x", d => {
+          return x(d[0]);
+        })
+        .attr("y", d => {
+          return y(d.data.country_name)
+        })
+        .attr("width", d  => x(d[1]) - x(d[0]))
+        .attr("height",y.bandwidth())
+        .on("mouseover", function (e, d) {
+          hover.value = {
+            cause: select(this.parentNode).data()[0].key,
+            detail: d.data
+          }
+          tooltip.value.style.top = `${e.clientY + 20}px`;
+          tooltip.value.style.left = `${e.clientX + 20}px`;
+        })
+        .on("mouseout", () => hover.value = null),
+      update => update
+        .attr("x", d => {
+          return x(d[0]);
+        })
+        .attr("y", d => {
+          return y(d.data.country_name)
+        })
+        .attr("width", d  => x(d[1]) - x(d[0]))
+        .attr("height",y.bandwidth())
+        .on("mouseover", function (e, d) {
+          hover.value = {
+            cause: select(this.parentNode).data()[0].key,
+            detail: d.data
+          }
+          tooltip.value.style.top = `${e.clientY + 20}px`;
+          tooltip.value.style.left = `${e.clientX + 20}px`;
+        })
+        .on("mouseout", () => hover.value = null),
+      exit => exit.remove()
+    )
+}
+
+
+
 </script>
+
 <template>
-  <!-- would be better to put this selector into a parent page and store the year and other data globally -->
-  <div class="year-selector-wrapper">
-    <div v-for="year in years"
-      :key="year"
-      :class="`year-wrapper${stackState.year === year ? ' active-year-wrapper' : ''}`"
-      @click="stackState.setYear(year)">
-      <div></div>
-      <p>{{year}}</p>
+    <!-- would be better to put this selector into a parent page and store the year and other data globally -->
+    <div class="year-selector-wrapper">
+        <div
+            v-for="year in years"
+            :key="year"
+            :class="`year-wrapper${
+                stackState.year === year ? ' active-year-wrapper' : ''
+            }`"
+            @click="onChangeYear(year)"
+        >
+            <div></div>
+            <p>{{ year }}</p>
+        </div>
     </div>
-  </div>
 
+    <div ref="wrapper">
+        <svg
+            ref="svg"
+            :width="stackState.width"
+            :height="stackState.width * 3"
+        ></svg>
+    </div>
 
-  <div ref="wrapper">
-    <svg ref="svg" :width="stackState.width" :height="stackState.width">
-      
-    </svg>
-  </div>
+    <Teleport to="body">
+      <div ref="tooltip" v-show="hover" class="tooltip">
+        <h4>{{hover?.detail.country_name}}</h4>
+        <p>Year: {{stackState.year}}</p>
+        <p>Cause: {{hover?.cause}}</p>
+        <p>Death: {{hover?.detail[hover?.cause]}}</p>
+      </div>
+    </Teleport>
 </template>
 
 <style scoped lang="scss">
 @mixin year-hover {
-  div {
-    border-width: 0.25rem;
-  }
-
-  p {
-    color: black;
-  }
-}
-
-.year-selector-wrapper {
-  display: flex;
-  gap: 1.5rem;
-  padding: 1.5rem;
-  overflow: auto;
-
-  .year-wrapper {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 1rem;
-    cursor: pointer;
-
-    div, p {
-      pointer-events: none;
-    }
-
     div {
-      box-sizing: border-box;
-      width: 1rem;
-      height: 1rem;
-      border-radius: 50%;
-      background-color: #E6A23C;
-      border: 0px solid black;
-      transition: all .3s;
+        border-width: 0.25rem;
     }
 
     p {
-      margin: 0;
-      color: gray;
-      transition: all .3s;
-      font-weight: bold;
+        color: black;
+    }
+}
+
+.year-selector-wrapper {
+    display: flex;
+    gap: 1.5rem;
+    padding: 1.5rem;
+    overflow: auto;
+
+    .year-wrapper {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 1rem;
+        cursor: pointer;
+
+        div,
+        p {
+            pointer-events: none;
+        }
+
+        div {
+            box-sizing: border-box;
+            width: 1rem;
+            height: 1rem;
+            border-radius: 50%;
+            background-color: #e6a23c;
+            border: 0px solid black;
+            transition: all 0.3s;
+        }
+
+        p {
+            margin: 0;
+            color: gray;
+            transition: all 0.3s;
+            font-weight: bold;
+        }
+
+        &:hover {
+            @include year-hover;
+        }
     }
 
-    &:hover {
-      @include year-hover
+    .active-year-wrapper {
+        @include year-hover;
     }
+}
+
+.tooltip {
+  background-color: white;
+  border-radius: 8px;
+  position: absolute;
+  padding: 2rem;
+  box-shadow: 0 0 10px rgba(0, 0, 0, .25);
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  h4, p {
+    margin: 0;
   }
 
-  .active-year-wrapper {
-    @include year-hover
+  p {
+    color: gray;
   }
 }
 </style>
--- src/stores/stack.js
+++ src/stores/stack.js
@@ -2,11 +2,13 @@ import { defineStore } from 'pinia';
 
 // import data from '../assets/data/cause_of_deaths.csv';
 // import meta from '../assets/data/meta_countries.csv';
+import deathReasonNames from '../assets/data/death_reasons.json'
+import deathReasons from '../assets/data/updated_death.json'
 
 export default defineStore('stack', {
   state: () => ({
     width: 0,
-    year: 1990
+    year: 1990,
   }),
 
   actions: {
@@ -16,6 +18,14 @@ export default defineStore('stack', {
 
     setYear(year) {
       this.year = year;
+    },
+
+    getYearlyData() {
+      return deathReasons[this.year];
+    },
+
+    getReasonNames() {
+      return deathReasonNames;
     }
   }
 });
