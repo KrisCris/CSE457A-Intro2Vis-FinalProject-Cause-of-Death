2d53b5207a0ed5cdf8ea129f8e34b3dccbd3e965
ChenXiao Liu <810196813@qq.com>
2022-11-21 18:08:17 -0600

    update bar chart

--- src/stores/barChart.js
+++ src/stores/barChart.js
@@ -1,31 +1,73 @@
 import { defineStore } from 'pinia';
+import { scaleBand, scaleSqrt, max } from 'd3';
+import causes from '../assets/data/reasons.json';
 
-// import data from '../assets/data/cause_of_deaths.csv';
-// import meta from '../assets/data/meta_countries.csv';
-import deathReasonNames from '../assets/data/death_reasons.json'
-import deathReasons from '../assets/data/reasons.json'
+let maxCount = 0;
+
+const setMaxCount = data => maxCount = max(data, d => d[0]);
 
 export default defineStore('barChart', {
-  state: () => ({
-    width: 0,
-    year: 1990,
-  }),
+  state: () => {
+    const year = 1990;
+    const base = 50;
+    const data = causes[year];
+
+    const domain = Object.keys(data);
+
+    const y = scaleBand()
+      .domain(domain)
+      .range([0, domain.length * base])
+      .padding([0.2]);
+
+    return {
+      year,
+      base,
+      y,
+      x: null,
+      data: Object.values(data),
+      order: 'unordered',
+    };
+  },
 
   actions: {
-    init(width) {
-      this.width = width;
+    setXScale(width) {
+      this.x = scaleSqrt()
+        .domain([0, setMaxCount(this.data)])
+        .range([0, width - 220]);
     },
-
     setYear(year) {
       this.year = year;
+      this.orderData();
+      this.x.domain([0, setMaxCount(this.data)]);
     },
-
-    getYearlyData() {
-      return deathReasons[this.year];
+    getCausesCount() {
+      return Object.keys(causes[this.year]).length;
     },
+    scaleColor(count) {
+      const low = maxCount / 4;
+      const middle = low * 2;
+      const high = low * 3;
 
-    getReasonNames() {
-      return deathReasonNames;
-    }
-  }
+      if (count < low) {
+        return '#99D594';
+      }
+      if (count < middle) {
+        return '#FEE08B';
+      }
+      if (count < high) {
+        return '#FC8D59';
+      }
+      return '#D53E4F';
+    },
+    orderData() {
+      const data = Object.values(causes[this.year]);
+      if (this.order === 'descending') {
+        data.sort((a, b) => b[0] - a[0]);
+      } else if (this.order === 'ascending') {
+        data.sort((a, b) => a[0] - b[0]);
+      }
+      this.y.domain(data.map(d => d[1]));
+      this.data = data;
+    },
+  },
 });
