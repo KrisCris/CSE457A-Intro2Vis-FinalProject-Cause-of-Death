4b55da6e913fdc217edf860dab8c7fee9e64f5a2
connlost <hi.imhpc@outlook.com>
2022-11-28 03:57:36 -0600

    add vue-virtual-scroller

--- src/main.js
+++ src/main.js
@@ -6,9 +6,13 @@ import Icon from './components/Icon.vue';
 // eslint-disable-next-line import/no-unresolved
 import 'virtual:svg-icons-register';
 import router from './router';
+import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
+import { RecycleScroller } from 'vue-virtual-scroller'
+
 
 createApp(App)
   .component('icon', Icon)
   .use(router)
   .use(createPinia())
+  .component('RecycleScroller', RecycleScroller)
   .mount('#app');
