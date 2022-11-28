9e802bbb4e785cba298df24f3c763b0688dc37a7
ChenXiao Liu <810196813@qq.com>
2022-11-28 11:46:07 -0600

    optimize code

--- src/pages/StackedBar/StackedBar.vue
+++ src/pages/StackedBar/StackedBar.vue
@@ -1,6 +1,6 @@
 <script setup>
 import {
-  ref, onMounted, reactive,
+  ref, onMounted, reactive, watch,
 } from 'vue';
 import stackStore from '../../stores/stack';
 import YearSelector from '../../components/YearSelector.vue';
@@ -15,42 +15,43 @@ const size = reactive({
   height: stack.base,
 });
 
+const visibleData = ref([]);
+const main = ref();
+const header = ref();
+
+const toggleVisibleData = () => {
+  const data = [];
+  const offset = header.value?.getBoundingClientRect().height;
+  const upper = main.value?.scrollTop ?? 0;
+  const lower = upper + window.innerHeight;
+
+  stack.data.forEach((d, i) => {
+    const top = i * stack.base + offset;
+    const bottom = top + stack.base;
+
+    if (!(bottom < upper || lower < top)) {
+      data.push([d, i]);
+    }
+  });
+  visibleData.value = data;
+};
+
 onMounted(() => {
   const { width } = wrapper.value.getBoundingClientRect();
   size.width = width;
   stack.setXScale(width);
+  toggleVisibleData();
+
+  watch(
+    () => stack.data,
+    () => toggleVisibleData(),
+  );
 });
 
-const main = ref();
-const header = ref();
-
-const isVisible = i => {
-  const offset = header.value?.getBoundingClientRect().height;
-  const upper = main.value?.scrollTop ?? 0;
-  const lower = upper + window.innerHeight;
-  const top = i * stack.base + offset;
-  const bottom = top + stack.base;
-
-  return (upper <= top && bottom <= lower)
-    || (top < upper && upper < bottom)
-    || (top < lower && lower < bottom);
-};
-
-const test = () => {
-  stack.containers.forEach((div, i) => {
-    if (!div) return;
-
-    if (isVisible(i)) {
-      div.classList.remove('hidden');
-    } else {
-      div.classList.add('hidden');
-    }
-  });
-};
 </script>
 
 <template>
-  <main @scroll="test" ref="main">
+  <main @scroll="toggleVisibleData" ref="main">
     <YearSelector :default="stack.year" @change="stack.setYear" />
     <section>
       <header ref="header">
@@ -74,59 +75,55 @@ const test = () => {
         </div>
       </header>
       <div ref="wrapper">
-        <div
-          class="container"
-          v-for="(country, i) in stack.data"
-          :key="country[0]"
+        <svg
+          :width="size.width"
+          :height="stack.base * stack.data.length"
         >
-          <svg
-            :width="size.width"
-            :height="stack.base"
-            :class="[{
-              hidden: !isVisible(i),
-            }]"
-            :ref="el => stack.containers[i] = el"
+          <g
+            v-for="[country, i] in visibleData"
+            :transform="`translate(0,${i * stack.base})`"
+            :key="country[0]"
           >
-              <text dominant-baseline="hanging">
-                {{country[1][0]}}
-              </text>
-              <text
-                dominant-baseline="hanging"
-                text-anchor="end"
-                fill="gray"
-                :x="size.width"
-              >
-                Total Death: {{country[1][2]}}
-              </text>
-              <Tooltip
-                v-for="d in stack.getStackedData(country[1])"
-                :key="d.key"
-              >
-                <template #target="{onMouseEnter, onMouseLeave}">
-                  <rect
-                    :fill="stack.scaleColor(d[0][1] - d[0][0])"
-                    stroke="white"
-                    :x="stack.x?.(d[0][0]) ?? 0"
-                    y="25"
-                    :width="stack.x?.(d[0][1] - d[0][0]) ?? 0"
-                    height="50"
-                    @mouseenter="onMouseEnter"
-                    @mouseleave="onMouseLeave"
-                  />
-                </template>
-                <template #content>
-                  <div class="tooltip-content">
-                    <h4>{{stack.meta[d.key]}}</h4>
-                    <p>Year: {{stack.year}}</p>
-                    <p>Population: {{country[1][1]}}</p>
-                    <p>Death: {{country[1][d.key]}}</p>
-                    <p>Death / Population: {{(country[1][3] * 100).toFixed(2)}}%</p>
-                    <p>Death / Total Death {{((d[0][1] - d[0][0]) * 100).toFixed(2)}}%</p>
-                  </div>
-                </template>
-              </Tooltip>
-          </svg>
-        </div>
+            <text dominant-baseline="hanging">
+              {{country[1][0]}}
+            </text>
+            <text
+              dominant-baseline="hanging"
+              text-anchor="end"
+              fill="gray"
+              :x="size.width"
+            >
+              Total Death: {{country[1][2]}}
+            </text>
+            <Tooltip
+              v-for="d in stack.getStackedData(country[1])"
+              :key="d.key"
+            >
+              <template #target="{onMouseEnter, onMouseLeave}">
+                <rect
+                  :fill="stack.scaleColor(d[0][1] - d[0][0])"
+                  stroke="white"
+                  :x="stack.x?.(d[0][0]) ?? 0"
+                  y="25"
+                  :width="stack.x?.(d[0][1] - d[0][0]) ?? 0"
+                  height="50"
+                  @mouseenter="onMouseEnter"
+                  @mouseleave="onMouseLeave"
+                />
+              </template>
+              <template #content>
+                <div class="tooltip-content">
+                  <h4>{{stack.meta[d.key]}}</h4>
+                  <p>Year: {{stack.year}}</p>
+                  <p>Population: {{country[1][1]}}</p>
+                  <p>Death: {{country[1][d.key]}}</p>
+                  <p>Death / Population: {{(country[1][3] * 100).toFixed(2)}}%</p>
+                  <p>Death / Total Death {{((d[0][1] - d[0][0]) * 100).toFixed(2)}}%</p>
+                </div>
+              </template>
+            </Tooltip>
+          </g>
+        </svg>
       </div>
     </section>
   </main>
@@ -165,10 +162,6 @@ rect {
 
 .hidden {
   display: none;
-}
-
-.container {
-  height: 100px;
 }
 
 .search-wrapper {
